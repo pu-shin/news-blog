ae8996fb71b815e9f285959c8442bd3c65f38b56
pu-shin <goldenfoxtheninja@gmail.com>
2022-11-10 15:32:40 +0400

    init firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyAeVV-T_D4gDxWzm6ZfjMZEYQpT4wtViBk",
+	authDomain: "pu-shin-news-blog-5996b.firebaseapp.com",
+	databaseURL: "https://pu-shin-news-blog-5996b-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "pu-shin-news-blog-5996b",
+	storageBucket: "pu-shin-news-blog-5996b.appspot.com",
+	messagingSenderId: "397782308566",
+	appId: "1:397782308566:web:ce11487ac64c776dde1b77"
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from './firebase'
 import './styles/global.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+
+createApp(App).use(store).use(router).use(firebase).mount('#app')
