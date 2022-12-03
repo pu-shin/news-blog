3310c3637e0a178d160c0e5d01ba8739f4d061a8
pu-shin <goldenfoxtheninja@gmail.com>
2022-12-03 20:21:51 +0400

    add default news

--- src/components/AppTopNews.vue
+++ src/components/AppTopNews.vue
@@ -1,26 +1,57 @@
 <template>
-  <section class="home">
-    <div class="home__container">
-      <div class="home__image">
-        <img src="../assets/images/japan-house.jpg" alt="foto news" />
+  <section class="top-news">
+    <div class="top-news__container preview__body">
+      <div class="top-news__image">
+        <img :src="previewNews.image" alt="" />
       </div>
-      <div class="home__content content-home">
-        <span class="content-home__label label">Illustration</span>
-        <h1 class="content-home__title title">
-          Japan House opens in mountainside to foster peak creativity.
+      <div class="top-news__content content-top-news">
+        <span class="content-top-news__label label">
+          {{ previewNews.type }}
+        </span>
+        <h1 class="content-top-news__title title">
+          {{ previewNews.title }}
         </h1>
-        <p class="content-home__text text">
-          Enim omittam qui id, ex quo atqui dictas complectitur. Nec ad timeam
-          accusata, hinc justo falli id eum, ferri novum molestie eos cu.
+        <p class="content-top-news__desc desc">
+          {{ previewNews.desc }}
         </p>
-        <span class="content-home__ author author">by Reta Torphy</span>
+        <span class="content-top-news__author author">
+          {{ previewNews.author }}
+        </span>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      staticNews: {
+        image: require("../assets/images/japan-house.jpg"),
+        type: "Illustration",
+        title: "Japan House opens in mountainside to foster peak creativity.",
+        desc: "Enim omittam qui id, ex quo atqui dictas complectitur. Nec ad timeam accusata, hinc justo falli id eum, ferri novum molestie eos cu.",
+        author: "by Reta Torphy",
+      },
+    };
+  },
+  methods: {},
+  computed: {
+    ...mapState(["news"]),
+    previewNews() {
+      const data = this.news.length ? this.news[0] : this.staticNews;
+      return {
+        image: data.image,
+        type: data.type,
+        title: data.title,
+        desc: data.desc,
+        author: data.author,
+      };
+    },
+  },
+};
 </script>
 
 
